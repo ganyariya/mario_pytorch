a414450190b1906279fd0c0a2c52810d33cb2147
ganariya <ganariya2525@gmail.com>
2021-08-25 08:24:01 +0900

    :memo: fix comment

--- mario_pytorch/main.py
+++ mario_pytorch/main.py
@@ -6,6 +6,7 @@ https://github.com/YuansongFeng/MadMario/blob/master/agent.py
 import datetime
 
 from pathlib import Path
+from os import path
 
 import torch
 import gym_super_mario_bros
@@ -25,31 +26,41 @@ WORLD: int = 1
 STAGE: int = 1
 VERSION: int = 3
 
+USE_CUDA = torch.cuda.is_available()
+IS_RENDER = True
+EPISODES = 10
+EVERY_RECORD = 20
+EVERY_RENDER = 20
+
+save_dir = (
+    Path(path.dirname(__file__)).parent
+    / "checkpoints"
+    / datetime.datetime.now().strftime("%Y-%m-%dT%H-%M-%S")
+)
+save_dir.mkdir(parents=True)
+
 # 4 Consecutive GrayScale Frames Set [4, 84, 84]
 env = gym_super_mario_bros.make(get_env_name(WORLD, STAGE, VERSION))
 env = JoypadSpace(env, SIMPLE_MOVEMENT)
 env = SkipFrame(env, skip=4)
 env = GrayScaleObservation(env)
 env = ResizeObservation(env, shape=84)
-env = FrameStack(env, num_stack=4)
-
-
-use_cuda = torch.cuda.is_available()
-is_render = True
-
-save_dir = Path("checkpoints") / datetime.datetime.now().strftime("%Y-%m-%dT%H-%M-%S")
-save_dir.mkdir(parents=True)
+env = FrameStack(env, num_stack=4)  # 4Frame まとめて取り出す
 
 mario = Mario(state_dim=(4, 84, 84), action_dim=env.action_space.n, save_dir=save_dir)
 logger = MetricLogger(save_dir)
 
-episodes = 10
-for e in range(episodes):
+for e in range(EPISODES):
+
+    # state.shape (4, 84, 84)
+    # state.frame_shape (84, 84)
     state = env.reset()
+
     while True:
         action = mario.act(state)
-        if is_render:
+        if IS_RENDER and e % EVERY_RENDER == 0:
             env.render()
+
         next_state, reward, done, info = env.step(action)
 
         mario.cache(state, next_state, action, reward, done)
@@ -63,5 +74,5 @@ for e in range(episodes):
             break
 
     logger.log_episode()
-    if e % 20 == 0:
+    if e % EVERY_RECORD == 0:
         logger.record(episode=e, epsilon=mario.exploration_rate, step=mario.curr_step)
