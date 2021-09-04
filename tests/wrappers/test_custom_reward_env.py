0d3c2498a3bc4823ab576d379813dc3556e62d73
ganariya <ganariya2525@gmail.com>
2021-09-04 14:01:37 +0900

    :white_check_mark: fix test

--- tests/wrappers/test_custom_reward_env.py
+++ tests/wrappers/test_custom_reward_env.py
@@ -7,3 +7,30 @@ def test_reward_x(make_env: CustomRewardEnv):
     reward = make_env.process_reward_x(info)
     assert reward == 10
     assert make_env.pprev_x == 10
+
+
+def test_reward_coin_normal(make_env: CustomRewardEnv):
+    info = {"coins": 10}
+    reward = make_env.process_reward_coin(info)
+    assert reward == 10
+    assert make_env.pprev_coin == 10
+
+
+def test_reward_coin_over(make_env: CustomRewardEnv):
+    info = {"coins": 98}
+    reward = make_env.process_reward_coin(info)
+    assert reward == 98
+    info = {"coins": 5}
+    reward = make_env.process_reward_coin(info)
+    assert reward == 7
+    assert make_env.pprev_coin == 5
+
+
+def test_reward_life(make_env: CustomRewardEnv):
+    info = {"life": np.array(2)}
+    reward = make_env.process_reward_life(info)
+    assert reward == 0
+    info = {"life": np.array(255)}
+    reward = make_env.process_reward_life(info)
+    assert reward == 3
+    assert make_env.pprev_life == -1
