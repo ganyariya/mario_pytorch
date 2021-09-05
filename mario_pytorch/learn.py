79fdeeeb33a2dcf0806037f8536636c325ba50ad
ganariya <ganariya2525@gmail.com>
2021-09-05 12:53:51 +0900

    simple change

--- mario_pytorch/learn.py
+++ mario_pytorch/learn.py
@@ -76,8 +76,6 @@ for e in range(config.EPISODES):
         q, loss = mario.learn()
 
         logger.log_step(reward, loss, q)
-        if reward > 0:
-            print(reward)
 
         state = next_state
 
