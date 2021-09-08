247c01f4f333c60f4ef5abc317697b96f0a03b42
ganariya <ganariya2525@gmail.com>
2021-09-08 13:59:27 +0900

    :white_check_mark: fix tests

--- tests/wrappers/test_custom_reward_env.py
+++ tests/wrappers/test_custom_reward_env.py
@@ -9,6 +9,8 @@ def test_x(make_env: CustomRewardEnv):
     diff_info = {"x_pos": diff_x}
     reward = make_env.process_reward_x(diff_info)
     assert reward == 10
+    make_env.update_pprev_x(info)
+    assert make_env.pprev_x == 10
 
 
 def test_kills(make_env: CustomRewardEnv):
@@ -18,90 +20,128 @@ def test_kills(make_env: CustomRewardEnv):
     diff_info = {"kills": diff_kills}
     reward = make_env.process_reward_kills(diff_info)
     assert reward == 10
+    make_env.update_pprev_kills(info)
+    assert make_env.pprev_kills == 10
 
 
-def test_reward_coin_normal(make_env: CustomRewardEnv):
+def test_normal_coins(make_env: CustomRewardEnv):
     info = {"coins": 10}
-    reward = make_env.process_reward_coin(info)
+    diff_coins = make_env.get_diff_coins(info)
+    assert diff_coins == 10
+    diff_info = {"coins": diff_coins}
+    reward = make_env.process_reward_coin(diff_info)
     assert reward == 10
-    # assert make_env.pprev_coin == 10
+    make_env.update_pprev_coin(info)
+    assert make_env.pprev_coin == 10
 
 
-def test_reward_coin_over(make_env: CustomRewardEnv):
-    info = {"coins": 98}
-    reward = make_env.process_reward_coin(info)
-    assert reward == 98
-    info = {"coins": 5}
-    reward = make_env.process_reward_coin(info)
+def test_over_coins(make_env: CustomRewardEnv):
+    make_env.update_pprev_coin({"coins": 95})
+    info = {"coins": 10}
+    diff_coins = make_env.get_diff_coins(info)
+    assert diff_coins == 15
+    diff_info = {"coins": diff_coins}
+    reward = make_env.process_reward_coin(diff_info)
+    assert reward == 15
+    make_env.update_pprev_coin(info)
+    assert make_env.pprev_coin == 10
 
 
-def test_reward_life(make_env: CustomRewardEnv):
-    pass
-    # info = {"life": np.array(2)}
-    # reward = make_env.process_reward_life(info)
-    # assert reward == 0
-    # info = {"life": np.array(255)}
-    # reward = make_env.process_reward_life(info)
-    # assert reward == 3
-    # assert make_env.pprev_life == -1
+def test_reward_life_add(make_env: CustomRewardEnv):
+    info = {"life": np.array(3)}
+    diff_life = make_env.get_diff_life(info)
+    assert diff_life == 1
+    diff_info = {"life": diff_life}
+    reward = make_env.process_reward_life(diff_info)
+    assert reward == 1
+    make_env.update_pprev_life(info)
+    assert make_env.pprev_life == 3
+
+
+def test_reward_life_dec(make_env: CustomRewardEnv):
+    info = {"life": np.array(1)}
+    diff_life = make_env.get_diff_life(info)
+    assert diff_life == -1
+    diff_info = {"life": diff_life}
+    reward = make_env.process_reward_life(diff_info)
+    assert reward == -1
+    make_env.update_pprev_life(info)
+    assert make_env.pprev_life == 1
+
+
+def test_reward_not_goal(make_env: CustomRewardEnv):
+    info = {"flag_get": 0}
+    diff_goal = make_env.get_diff_goal(info)
+    assert diff_goal == 0
+    diff_info = {"goal": 0}
+    reward = make_env.process_reward_goal(diff_info)
+    assert reward == 0
 
 
 def test_reward_goal(make_env: CustomRewardEnv):
-    info = {"goal": 0}
-    reward = make_env.process_reward_goal(info)
-    assert reward == 0
-    info = {"goal": 1}
-    reward = make_env.process_reward_goal(info)
+    info = {"flag_get": 1}
+    diff_goal = make_env.get_diff_goal(info)
+    assert diff_goal == 1
+    diff_info = {"goal": 1}
+    reward = make_env.process_reward_goal(diff_info)
     assert reward == 1
 
 
 def test_reward_item_plus(make_env: CustomRewardEnv):
-    pass
-    # info = {"status": "tall"}
-    # reward = make_env.process_reward_item(info)
-    # assert reward == 1
-    # info = {"status": "fireball"}
-    # reward = make_env.process_reward_item(info)
-    # assert reward == 1
+    info = {"status": "tall"}
+    diff_item = make_env.get_diff_item(info)
+    assert diff_item == 1
+    diff_info = {"item": 1}
+    reward = make_env.process_reward_item(diff_info)
+    assert reward == 1
+    make_env.update_pprev_status(info)
+    assert make_env.pprev_status == 1
 
 
 def test_reward_item_minus(make_env: CustomRewardEnv):
-    pass
-    # info = {"status": "fireball"}
-    # reward = make_env.process_reward_item(info)
-    # assert reward == 2
-    # info = {"status": "small"}
-    # reward = make_env.process_reward_item(info)
-    # assert reward == 0
+    make_env.update_pprev_status({"status": "tall"})
+    info = {"status": "small"}
+    diff_item = make_env.get_diff_item(info)
+    assert diff_item == -1
+    diff_info = {"item": -1}
+    reward = make_env.process_reward_item(diff_info)
+    assert reward == -1
+    make_env.update_pprev_status(info)
+    assert make_env.pprev_status == 0
 
 
-def test_reward_time_plus(make_env: CustomRewardEnv):
+def test_reward_time(make_env: CustomRewardEnv):
     make_env.reset()
     info = {"time": 300}
-    reward = make_env.process_reward_time(info)
-    # assert reward == 100
-
-
-def test_reward_time_minus(make_env: CustomRewardEnv):
-    make_env.reset()
-    info = {"time": 450}
-    reward = make_env.process_reward_time(info)
-    # assert reward == 0
+    diff_time = make_env.get_diff_time(info)
+    assert diff_time == -100
+    diff_info = {"time": diff_time}
+    reward = make_env.process_reward_time(diff_info)
+    assert reward == -100
+    make_env.update_pprev_time(info)
+    assert make_env.pprev_time == 300
 
 
 def test_reward_time_score(make_env: CustomRewardEnv):
-    info = {"score": 10}
-    reward = make_env.process_reward_score(info)
-    assert reward == 10
+    make_env.reset()
+    info = {"score": 300}
+    diff_score = make_env.get_diff_score(info)
+    assert diff_score == 300
+    diff_info = {"score": diff_score}
+    reward = make_env.process_reward_score(diff_info)
+    assert reward == 300
+    make_env.update_pprev_score(info)
+    assert make_env.pprev_score == 300
 
 
 def test_reset_on_each_life(make_env: CustomRewardEnv):
     # not work
     info = {"life": np.array(2), "x_pos": np.array(100), "time": 500}
     make_env.reset_on_each_life(info)
-    # assert make_env.pprev_x == 0
+    assert make_env.pprev_x == 0
 
+    # work
     info = {"life": np.array(0), "x_pos": np.array(100), "time": 500}
     make_env.reset_on_each_life(info)
-    # assert make_env.pprev_x == 100
-    # assert make_env.pprev_time == 500
+    assert make_env.pprev_x == 100
+    assert make_env.pprev_time == 500
